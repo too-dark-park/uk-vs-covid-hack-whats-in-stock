f763d1d4a80dc002b97f667e2341cc2e1ef388eb
Evie Skinner <evie.skinner@teamriver.com>
2020-04-11 14:00:34 +0100

    Added Models and Services dirs. Added DB context

---
+++ WhatsIn/WhatsIn/Models/WhatsInContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WhatsIn.Models
+{
+    public class WhatsInContext : DbContext
+    {
+        public WhatsInContext(DbContextOptions options) : base(options) { }
+
+        // each DB Set is a table
+        public DbSet<Product> Products { get; set; }
+    }
+}
