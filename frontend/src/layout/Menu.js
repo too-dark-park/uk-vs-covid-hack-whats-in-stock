18e6cd2610939b4ca70185f7a60467acea15a71c
bella-cockrell <heybellac@gmail.com>
2020-04-11 19:44:57 +0100

    Adding Menu.js

---
+++ frontend/src/layout/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import menu_icon from './menu.png';
+
+export default function Menu() {
+
+  return (
+    <>
+      <nav className='menu'>
+        <img className='icon' src={menu_icon} alt='' />
+      </nav>
+    </>
+  );
+
+}
+
