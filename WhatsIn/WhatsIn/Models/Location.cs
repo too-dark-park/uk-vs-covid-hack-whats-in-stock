d81f31a7e90a905766d112487d3777346bb9d23b
Evie Skinner <evie.skinner@teamriver.com>
2020-04-11 16:00:07 +0100

    Added Product, Location and User domain models

---
+++ WhatsIn/WhatsIn/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿namespace WhatsIn.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        // e.g supermarket, café, shop
+        public string Type { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
--- WhatsIn/WhatsIn/Models/Product.cs
+++ WhatsIn/WhatsIn/Models/Product.cs
@@ -7,5 +7,12 @@ namespace WhatsIn.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Location ProductLocation { get; set; }
     }
 }
