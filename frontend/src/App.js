43f7f1158223103c30b56e54a8a3ecb68de9321d
bella-cockrell <heybellac@gmail.com>
2020-04-11 12:30:46 +0100

    Move frontend to root

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          WhatsIN
+        </h1>
+        <h3>
+          Shopping!
+        </h3>
+        <p>
+          Low on toilet paper but the queues outside the shop are too long?
+          Save yourself a trip and find what's in stock before you leave the house.
+        </p>
+      </header>
+      <footer>
+        <a
+          className="App-link"
+          href="https://github.com/too-dark-park/uk-vs-covid-hack-whats-in-stock"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to our GitHub
+          </a>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
