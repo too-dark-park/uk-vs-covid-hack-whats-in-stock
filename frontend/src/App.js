6a0f9387e4c9fdea40ebe1c8da752564c2a2b9cb
RainbowGiantSquid <dagmarakukla@gmail.com>
2020-04-11 17:08:47 +0100

    Working data fetch from example endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,72 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          WhatsIN
-        </h1>
-        <h3>
-          Shopping!
-        </h3>
-        <p>
-          Low on toilet paper but the queues outside the shop are too long?
-          Save yourself a trip and find what's in stock before you leave the house.
-        </p>
-      </header>
-      <footer>
-        <a
-          className="App-link"
-          href="https://github.com/too-dark-park/uk-vs-covid-hack-whats-in-stock"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link to our GitHub
+class App extends React.Component {
+  _isMounted = false;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: this.getJson(),
+      error: false,
+    };
+  }
+
+  async getJson() {
+    const linkJson =
+      "https://whatsin.whiscode.dotnetcloud.co.uk/WeatherForecast";
+    const fetchJson = await fetch(linkJson, { cache: "no-cache" });
+    const dataJson = await fetchJson.json();
+    if (this._isMounted) {
+      this.setState({ data: dataJson });
+    }
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this.getJson = this.getJson.bind(this);
+    this.getJson();
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ error: true });
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    const { data, error } = this.state;
+    return error ? (
+      <div>Ooops... something went wrong.</div>
+    ) : (
+      <div className="App">
+        {data && console.log(this.state.data)}
+        <header className="App-header">
+          <h1>WhatsIN</h1>
+          <h3>Shopping!</h3>
+          https://whatsin.whiscode.dotnetcloud.co.uk/WeatherForecast
+          <p>
+            Low on toilet paper but the queues outside the shop are too long?
+            Save yourself a trip and find what's in stock before you leave the
+            house.
+          </p>
+        </header>
+        <footer>
+          <a
+            className="App-link"
+            href="https://github.com/too-dark-park/uk-vs-covid-hack-whats-in-stock"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Link to our GitHub
           </a>
-      </footer>
-
-    </div>
-  );
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
