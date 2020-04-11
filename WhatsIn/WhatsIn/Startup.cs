4f9036878bf0f458787c69c1324a91d216ac917f
Evie Skinner <evie.skinner@teamriver.com>
2020-04-11 15:50:38 +0100

    Added DB context using conn string hidden in environment variable

--- WhatsIn/WhatsIn/Startup.cs
+++ WhatsIn/WhatsIn/Startup.cs
@@ -1,7 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using dotenv.net;
+using dotenv.net.DependencyInjection.Microsoft;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WhatsIn.Models;
 
 namespace WhatsIn
 {
@@ -25,7 +27,24 @@ namespace WhatsIn
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // set up dotenv to grab the env vars
+            DotEnv.Config();
+            services.AddEnv(builder => {
+                builder
+                .AddEnvFile("./.env")
+                .AddThrowOnError(false)
+                .AddEncoding(Encoding.ASCII);
+            });
+
+            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
             services.AddControllers();
+
+            // add the DB context
+            services.AddDbContext<WhatsInContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddSingleton(Configuration);
+
+            // add any new services to the collection here
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
